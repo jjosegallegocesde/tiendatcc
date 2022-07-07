1d6589287f05283e0b6ff83b411aeb7b59e2196e
jjosegallegomesa <juan.gallegomesa@gmail.com>
2022-07-07 08:51:05 -0500

    se crea bd

--- src/controllers/controladorLlenado.js
+++ src/controllers/controladorLlenado.js
@@ -2,12 +2,42 @@
 let productosBD=[
 
     {
-        nombre:"Funko dexter",
+        nombre:"Funko dexter-Dexter 1067",
         precio:55000,
         cantidad:0,
-        fotos:['https://firebasestorage.googleapis.com/v0/b/jjgtiendatcc.appspot.com/o/funko1.jpg?alt=media&token=f228f3d9-84d2-4c63-a35c-cc6309cea056','https://firebasestorage.googleapis.com/v0/b/jjgtiendatcc.appspot.com/o/funko1.jpg?alt=media&token=f228f3d9-84d2-4c63-a35c-cc6309cea056'],
-        descripcion:'figura del laboratorio de dexter'
+        fotos:['https://firebasestorage.googleapis.com/v0/b/jjgtiendatcc.appspot.com/o/funko1.jpg?alt=media&token=f228f3d9-84d2-4c63-a35c-cc6309cea056','https://firebasestorage.googleapis.com/v0/b/jjgtiendatcc.appspot.com/o/funko11.jpg?alt=media&token=35f4b12d-1427-4909-8e7c-ab790f2714e4'],
+        descripcion:'Figura del laboratorio de dexter CartoonNetwork'
     },
-    {}
+    {
+        nombre:"Funko Freezer-Dragon Ball Z 867",
+        precio:54900,
+        cantidad:0,
+        fotos:['https://firebasestorage.googleapis.com/v0/b/jjgtiendatcc.appspot.com/o/funko2.jpg?alt=media&token=42083282-5e88-4f59-b914-7d106a4e2f12','https://firebasestorage.googleapis.com/v0/b/jjgtiendatcc.appspot.com/o/funko12.webp?alt=media&token=a3046179-db9d-42f1-adc4-a3b625ed836c'],
+        descripcion:'Figura Dragon ball z saga Freezer'
+
+    },
+    {
+
+        nombre:"Funko Gohan SSY2-Dragon Ball Z 858",
+        precio:54900,
+        cantidad:0,
+        fotos:['https://firebasestorage.googleapis.com/v0/b/jjgtiendatcc.appspot.com/o/funko3.jpg?alt=media&token=3b1ed74b-e2f2-462d-9103-1c2239441ba4','https://firebasestorage.googleapis.com/v0/b/jjgtiendatcc.appspot.com/o/funo13.webp?alt=media&token=1736bae4-9d07-4a75-ad7e-30e2d0877b5a'],
+        descripcion:'Figura Dragon ball z saga majin boo'
+
+    },
+    {
+        nombre:"Funko Bulbasaur-Pokemon 453",
+        precio:63900,
+        cantidad:0,
+        fotos:['https://firebasestorage.googleapis.com/v0/b/jjgtiendatcc.appspot.com/o/funko4.jpg?alt=media&token=11551439-630c-4ebf-8b70-21be30494633','https://firebasestorage.googleapis.com/v0/b/jjgtiendatcc.appspot.com/o/funko4.jpg?alt=media&token=11551439-630c-4ebf-8b70-21be30494633'],
+        descripcion:'Figura POKEMON color original'
+    },
+    {
+        nombre:"Funko Frodo-Lord od the rings 444",
+        precio:63900,
+        cantidad:0,
+        fotos:['https://firebasestorage.googleapis.com/v0/b/jjgtiendatcc.appspot.com/o/funko5.jpg?alt=media&token=9f6f74a0-0a00-4d22-9d2b-4057e45ec6f6','https://firebasestorage.googleapis.com/v0/b/jjgtiendatcc.appspot.com/o/funko5.jpg?alt=media&token=9f6f74a0-0a00-4d22-9d2b-4057e45ec6f6'],
+        descripcion:'Figura Frodo Baggins con capa elfica' 
+    }
 
 ]
