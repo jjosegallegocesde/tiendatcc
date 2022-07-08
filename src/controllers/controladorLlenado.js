0e8be66346bd214398fd304da3be678c0cc3726e
Albeiro Muriel <j.albeiromuriel@gmail.com>
2022-07-08 11:16:19 -0500

    se crea tienda

--- src/controllers/controladorLlenado.js
+++ src/controllers/controladorLlenado.js
@@ -41,6 +41,56 @@ let productosBD=[
 
 ]
 
+//PASOS PARA PINTAR (HACER RENDER) DE ETIQUETAS HTML DESDE JS
+//TRAVERSING
+
+//1. crear una referencia en memoria(variable) que almacena
+//la etiquea sobre la cual voy a pintar (ETIQUETA PADRE O ETIQUETA RAIZ)
+let fila=document.getElementById("fila")
+
+
+//2. RECORRO EL ARREGLO DE DATOS Y APLICO TRAVERSING
+productosBD.forEach(function(producto){
+
+    //2.1 creando una columna desde JS
+    let columna=document.createElement("div")
+    columna.classList.add("col")
+
+    //2.2 Creando la tarjeta
+    let tarjeta=document.createElement("div")
+    tarjeta.classList.add("card","h-100","text-center")
+
+    //2.3 creando la foto
+    let fotoProducto=document.createElement("img")
+    fotoProducto.classList.add("img-fluid","w-100")
+    fotoProducto.src=producto.fotos[0]
+
+    //2.4 creando el nombre del producto
+    let nombreProducto=document.createElement("h3")
+    nombreProducto.textContent=producto.nombre
+
+    //2.5 creando el precio del producto
+    let precioProducto=document.createElement("h2")
+    precioProducto.classList.add("fw-bold")
+    precioProducto.textContent='$'+producto.precio+'COP'
+
+    //2.6 DETECTANDO EVENTOS
+    columna.addEventListener("mouseover",function(){
+        fotoProducto.src=producto.fotos[1]
+    })
+
+    columna.addEventListener("mouseleave",function(){
+        fotoProducto.src=producto.fotos[0]
+    })
+
+    //3 DEFINO PADRES E HIJOS 
+    tarjeta.appendChild(fotoProducto)
+    tarjeta.appendChild(nombreProducto)
+    tarjeta.appendChild(precioProducto)
+    columna.appendChild(tarjeta)
+    fila.appendChild(columna)
+    
 
 
 
+})
